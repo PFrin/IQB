7c3ac3677c3bfb2005195f791a371ca855e43631
Malo Rihet <malo.rihet@gmail.com>
2023-06-13 17:36:09 +0200

    script init table type
    a tester

--- manage.py
+++ manage.py
@@ -2,6 +2,9 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import django
+from django.conf import settings
+from IQB.models import Type
 
 
 def main():
@@ -17,6 +20,23 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
+def initialize_table():
+    # Vérifiez d'abord si des enregistrements existent déjà dans la table
+    if Type.objects.exists():
+        print("La table 'Type' existe déjà. Aucune initialisation nécessaire.")
+        return
+    
+    # Créez les objets Type avec les valeurs spécifiées
+    type_1 = Type.objects.create(typeQuestion="Question à échelle")
+    type_2 = Type.objects.create(typeQuestion="question ouverte")
+    type_3 = Type.objects.create(typeQuestion="choix multiple")
+    type_4 = Type.objects.create(typeQuestion="Choix unique")
+
+    print("Initialisation de la table 'Type' terminée.")
 
 if __name__ == '__main__':
     main()
+
+    initialize_table()
+
+    execute_from_command_line(sys.argv)
