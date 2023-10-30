1cd819590b2294e2ddaec8bcc44be1104642795c
Malo Rihet <malo.rihet@gmail.com>
2023-10-30 04:48:30 +0100

    init BDD with sqlite3
    supression des migration
    changer de base de donnée car probleme avec mysql

--- IQB/settings.py
+++ IQB/settings.py
@@ -75,12 +75,12 @@ WSGI_APPLICATION = 'IQB.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 #DATABASES = {
 #    'default': {
@@ -95,19 +95,19 @@ WSGI_APPLICATION = 'IQB.wsgi.application'
 #}
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'data_ibq',
-        'USER': 'root',
-        'PASSWORD': 'IODAIQB2023',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
-    },
-} 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'data_ibq',
+#        'USER': 'root',
+#        'PASSWORD': 'IODAIQB2023',
+#        'HOST': '127.0.0.1',
+#        'PORT': '3306',
+#       'OPTIONS': {
+#            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+#       },
+#   },
+#} 
 
 
 # Password validation
@@ -147,6 +147,7 @@ DATE_INPUT_FORMATS = ('%d/%m/%Y','%Y-%m-%d')
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_URL_STYLE = '/static/IQB/css/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
