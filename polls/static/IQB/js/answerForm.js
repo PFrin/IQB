418645006fdc1393681c6659b0c005e30962eaef
Malo Rihet <malo.rihet@gmail.com>
2023-12-04 17:49:33 +0100

    Update answerForm.js

--- polls/static/IQB/js/answerForm.js
+++ polls/static/IQB/js/answerForm.js
@@ -2,6 +2,11 @@
 var curentPage = loadCurrentPage();
 var pageMax = loadPageMax();
 
+////////////////////////////
+// Charger les variables //
+///////////////////////////
+
+
 //charger la page actuelle à partir de la session
 function loadCurrentPage() {
     var currentPage = sessionStorage.getItem(`${formId}_currentPage`);
@@ -25,4 +30,135 @@ function loadPageMax() {
     }
     savePageMax(2);
     return 2; // Valeur par défaut pour la première page
+}
+
+///////////////////////////////
+// Sauvegarder les variables //
+///////////////////////////////
+
+function saveCurrentPage(pageNumber) {
+    sessionStorage.setItem(`${formId}_currentPage`, pageNumber);
+}
+
+function savePageMax(pageMax) {
+    console.log("savePageMax : "+pageMax);
+    sessionStorage.setItem(`${formId}_pageMax`, pageMax);
+}
+
+
+/////////////////////
+// Changer de Page //
+/////////////////////
+
+// btn page suivante
+function goToNextPage() {
+    var currentPage = loadCurrentPage();
+    var pageMax = loadPageMax();
+    
+    if (currentPage < totalPages && currentPage < pageMax) {
+      currentPage = currentPage+1;
+      pageMax = pageMax+1;
+      console.log("currentPage : "+currentPage);
+      saveCurrentPage(currentPage);
+      savePageMax(pageMax)
+  
+      showCurrentPage(currentPage);
+  
+    }
+    else{
+      console.log("ALEDDDDDDDDD");
+    }
   }
+// btn Page précédente
+function goToPreviousPage() {
+    var currentPage = loadCurrentPage();
+    if (currentPage > 1) {
+  
+      currentPage = currentPage-1;
+  
+      saveCurrentPage(currentPage);
+  
+      showCurrentPage(currentPage);
+    }
+  }
+
+
+//////////////////////////////////
+// Autorisation changer de Page //
+//////////////////////////////////
+
+// Toutes question obligatoire sont répondues
+  function requireAutorisation() {
+    const requiredQuestions = form.querySelectorAll('h3.obligatory');
+
+  }
+
+
+//////////////////////////
+// Fonction d'affichage //
+//////////////////////////
+
+// Afficher la page actuelle
+function showCurrentPage(currentPage) {
+    var pages = document.querySelectorAll('.page');
+    for (var i = 0; i < pages.length; i++) {
+      if (i === currentPage - 1) {
+        pages[i].style.display = 'block';
+      } else {
+        pages[i].style.display = 'none';
+      }
+    }
+    
+    // Gérer l'affichage des boutons "Page précédente" et "Page suivante"
+    var prevButton = document.getElementById('prevPage');
+    var nextButton = document.getElementById('nextPage');
+    var submitButton = document.getElementById('submitForm');
+  
+    console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaa");
+    console.log("currentPage : "+currentPage);
+    if (currentPage == 1) {
+      prevButton.style.display = 'none';
+    }
+    else{
+      prevButton.style.display = 'block';
+      prevButton.disabled = false;
+    }
+  
+    if (currentPage === totalPages) {
+      nextButton.style.display = 'none';
+      submitButton.style.display = 'block';
+    } else {
+      nextButton.style.display = 'block';
+      submitButton.style.display = 'none';
+    }
+  }
+
+//afficher les questions 
+
+
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+/*
+Charger les variables
+Sauvegarder les variables
+
+Affuichage : 
+    Afficher la page actuelle
+        P0   : intro 
+        PN   : form
+        PN+1 : conclusion
+    Afficher les questions de la page actuelle
+        Affichage des questions en focntion des dépendances 
+    Afficher les boutons de navigation 
+
+
+Changer de Page
+    suivante
+        
+        btn page suivante
+    précédente
+        btn page précédente
+
+Autorisation page suivante
+    Toutes question obligatoire sont répondues ( Question dépendance obligatoirement obligatoire)
+   
+*/
