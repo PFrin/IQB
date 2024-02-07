16c46c2ed7d960b5f05b24b969edafd508b81927
Malo Rihet <malo.rihet@gmail.com>
2024-02-07 17:23:02 +0100

    header

--- polls/static/IQB/js/master.js
+++ polls/static/IQB/js/master.js
@@ -3,3 +3,25 @@ function MesFormulaires(){
     redirectUrl = '/' + loginCust;
     document.location.href = redirectUrl;
 }
+
+function newForm(myCustomer) {
+    console.log(myCustomer.loginCust);
+    $.ajax({
+        type: 'POST',
+        url: '{% url "details" loginCust=myCustomer.loginCust %}',
+        data: {
+            'csrfmiddlewaretoken': '{{ csrf_token }}',
+            'action': 'newForm',
+            'Customer': myCustomer
+        },
+        success: function(response) {
+            if (response.success) {
+                //Redirection vers la page qui cherche le formulaire
+                var redirectUrl = '{% url "redirection" loginCust=myCustomer.loginCust %}';
+                document.location.href = redirectUrl;
+            } else {
+                console.log("Échec");
+            }
+        }
+    });
+}
