5ab6b63683ebde98b08cd6c92aa7942fe34555be
Malo Rihet <malo.rihet@gmail.com>
2024-02-08 09:58:56 +0100

    header fonctionel
    enlever "new form" si on en créer un

--- polls/static/IQB/js/master.js
+++ polls/static/IQB/js/master.js
@@ -4,24 +4,3 @@ function MesFormulaires(){
     document.location.href = redirectUrl;
 }
 
-function newForm(myCustomer) {
-    console.log(myCustomer.loginCust);
-    $.ajax({
-        type: 'POST',
-        url: '{% url "details" loginCust=myCustomer.loginCust %}',
-        data: {
-            'csrfmiddlewaretoken': '{{ csrf_token }}',
-            'action': 'newForm',
-            'Customer': myCustomer
-        },
-        success: function(response) {
-            if (response.success) {
-                //Redirection vers la page qui cherche le formulaire
-                var redirectUrl = '{% url "redirection" loginCust=myCustomer.loginCust %}';
-                document.location.href = redirectUrl;
-            } else {
-                console.log("Échec");
-            }
-        }
-    });
-}
